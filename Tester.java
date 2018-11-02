b84027166866efed37fe9775e5ba4fd2f54eed85
angeliquecd <acharles-davis00@stuy.edu>
2018-11-02 10:09:26 -0400

    added small tester to test code

---
+++ Tester.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Tester{
+  public static void main(String[] args){
+    ArrayList<String> listy = new ArrayList<String>();
+    System.out.println(listy.add("hello"));
+      System.out.println(listy.add(1,"hello"));
+  }
+}
