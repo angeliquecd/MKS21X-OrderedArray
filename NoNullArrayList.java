1532bebbfe45b5b217fc681bb0b4695cc6380b71
angeliquecd <acharles-davis00@stuy.edu>
2018-11-04 14:21:33 -0500

    added to nonull add(index,value)

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -9,11 +9,14 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   }
   public boolean add(T value){
     if (value==null) throw new IllegalArgumentException("Messed up.");
-    super.add(value);
+    return super.add(value);
   }
   public T set(int index, T value){
     if (value==null) throw new IllegalArgumentException("Messed up");
-    super.set(index, value);
-    return this.get(index);
+    return super.set(index, value);
+  }
+  public void add(int index, T value){
+    if (value==null) throw new IllegalArgumentException("Messed up");
+    super.add(index, value);
   }
 }
