049054406272cf42bdebb73ddb62294723308764
angeliquecd <acharles-davis00@stuy.edu>
2018-11-04 21:28:26 -0500

    fixed indexfinder to work for smallest values being added

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -12,17 +12,19 @@ public void add(int index, T value){
   add(value);
 }
 public boolean add(T value){
-   super.add(findindexy(value,this),value);
+  if (this.size()==0) super.add(value);
+  else super.add(findindexy(value,this),value);
    return true;
 }
 private int findindexy(T value, OrderedArrayList<T> listy){
   boolean firstly=(value.compareTo(listy.get(0)))>0;
   for (int i = 0;i<listy.size();i++){
     if((value.compareTo(listy.get(i))>0)!=firstly){
-      return listy.indexOf(listy.get(i));
+      return i;
     }
   }
-  return this.size();
+  if (!firstly) return 0;
+  else return this.size();
 }
 public T set(int index, T value){
   this.remove(index);
