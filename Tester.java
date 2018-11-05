8d9e5f8eee82784e4a0dd2755fb447ac6a687668
angeliquecd <acharles-davis00@stuy.edu>
2018-11-04 19:25:15 -0500

    added a few test cases to tester

--- Tester.java
+++ Tester.java
@@ -4,6 +4,23 @@ public class Tester{
   public static void main(String[] args){
     ArrayList<String> listy = new ArrayList<String>();
     System.out.println(listy.add("hello"));
-      System.out.println(listy.add(1,"hello"));
-  }
-}
+      //System.out.println(listy.add(1,"hello"));
+      String c = "4";
+      String d = "6";
+      Integer a = new Integer(4);
+      Integer b = new Integer(9);
+     System.out.println(a.compareTo(b));
+    String s1="hello";
+String s2="hello";
+String s3="meklo";
+String s4="hemlo";
+String s5="flag";
+OrderedArrayList<String> num = new OrderedArrayList<String>();
+OrderedArrayList<String> nums= new OrderedArrayList<String>();
+nums.add("hi");
+nums.add("yo");
+System.out.println(s1.compareTo(s2));//0 because both are equal
+System.out.println(s1.compareTo(s3));//-5 because "h" is 5 times lower than "m"
+System.out.println(s1.compareTo(s4));//-1 because "l" is 1 times lower than "m"
+System.out.println(s1.compareTo(s5));//2 because "h" is 2 times greater than "f"
+}}
